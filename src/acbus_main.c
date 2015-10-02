81dafeb6ecfca65b93f9d5dc143b5ba2e5756969
seb <asplendidday@gmail.com>
2015-10-02 20:00:06 +0200

    Redesign of bus data presentation

--- src/acbus_main.c
+++ src/acbus_main.c
@@ -10,8 +10,82 @@
 static Window* s_main_window = NULL;
 static TextLayer* s_bus_station = NULL;
 static TextLayer* s_next_station = NULL;
-static TextLayer* s_busses = NULL;
 static BitmapLayer* s_banner = NULL;
+
+#define NUM_BUSES 7
+#define BUS_ENTRY_MARGIN_TOP 28
+#define BUS_ENTRY_MARGIN_LEFT 3
+#define BUS_ENTRY_HEIGHT 16
+#define BUS_ENTRY_LINE_WIDTH 18
+#define BUS_ENTRY_DEST_WIDTH 102
+#define BUS_ENTRY_ETA_WIDTH 18
+    
+struct {
+    TextLayer* line;
+    TextLayer* dest;
+    TextLayer* eta;
+} s_buses[ NUM_BUSES ];
+
+static void create_text_layer( TextLayer** text_layer, GRect rect, GColor back_color, GColor text_color, const char* font_name, GTextAlignment text_align )
+{
+    *text_layer = text_layer_create( rect );
+    
+    text_layer_set_background_color( *text_layer, back_color );
+    text_layer_set_text_color( *text_layer, text_color );
+    text_layer_set_font( *text_layer, fonts_get_system_font( font_name ) );
+    text_layer_set_text_alignment( *text_layer, text_align );
+    text_layer_set_text( *text_layer, "" );
+    
+    layer_add_child( window_get_root_layer( s_main_window ), text_layer_get_layer( *text_layer ) ); 
+}
+
+static GRect line_rect( int index )
+{
+    return GRect( BUS_ENTRY_MARGIN_LEFT,
+                  BUS_ENTRY_MARGIN_TOP + index * BUS_ENTRY_HEIGHT,
+                  BUS_ENTRY_LINE_WIDTH,
+                  BUS_ENTRY_HEIGHT );
+}
+
+static GRect dest_rect( int index )
+{
+    return GRect( BUS_ENTRY_MARGIN_LEFT + BUS_ENTRY_LINE_WIDTH,
+                  BUS_ENTRY_MARGIN_TOP + index * BUS_ENTRY_HEIGHT,
+                  BUS_ENTRY_DEST_WIDTH,
+                  BUS_ENTRY_HEIGHT );
+}
+
+static GRect eta_rect( int index )
+{
+    return GRect( BUS_ENTRY_MARGIN_LEFT + BUS_ENTRY_LINE_WIDTH + BUS_ENTRY_DEST_WIDTH,
+                  BUS_ENTRY_MARGIN_TOP + index * BUS_ENTRY_HEIGHT,
+                  BUS_ENTRY_ETA_WIDTH,
+                  BUS_ENTRY_HEIGHT );
+}
+
+static void create_bus_text_layers()
+{    
+    for( int i = 0; i < NUM_BUSES; ++i )
+    {
+        create_text_layer( &s_buses[ i ].line, line_rect( i ), GColorWhite, GColorBlack, FONT_KEY_GOTHIC_14_BOLD, GTextAlignmentLeft );
+        create_text_layer( &s_buses[ i ].dest, dest_rect( i ), GColorWhite, GColorBlack, FONT_KEY_GOTHIC_14, GTextAlignmentLeft );
+        create_text_layer( &s_buses[ i ].eta, eta_rect( i ), GColorWhite, GColorBlack, FONT_KEY_GOTHIC_14_BOLD, GTextAlignmentRight );
+        
+        text_layer_set_text( s_buses[ i ].line, "33" );
+        text_layer_set_text( s_buses[ i ].dest, "Bushof Vaals oder nicht?" );
+        text_layer_set_text( s_buses[ i ].eta, "15" );
+    }
+}
+
+static void destroy_bus_text_layers()
+{
+    for( int i = 0; i < NUM_BUSES; ++i )
+    {
+        text_layer_destroy( s_buses[ i ].line );
+        text_layer_destroy( s_buses[ i ].dest );
+        text_layer_destroy( s_buses[ i ].eta );
+    }
+}
 
 
 static void update_proc(Layer *layer, GContext *ctx) {
@@ -22,34 +96,12 @@ static void update_proc(Layer *layer, GContext *ctx) {
 
 static void main_window_load()
 {
- 
-    s_bus_station = text_layer_create( GRect( 24, 0, 144, 20 ) );
-    text_layer_set_background_color( s_bus_station, GColorDarkCandyAppleRed );
-    text_layer_set_text_color( s_bus_station, GColorWhite );
-    text_layer_set_font( s_bus_station, fonts_get_system_font( FONT_KEY_GOTHIC_18_BOLD ) );
+    create_text_layer( &s_bus_station, GRect( 24, 0, 120, 20 ), GColorDarkCandyAppleRed, GColorWhite, FONT_KEY_GOTHIC_18_BOLD, GTextAlignmentLeft );
     text_layer_set_text( s_bus_station, "Waiting for first update..." );
-    text_layer_set_text_alignment( s_bus_station, GTextAlignmentLeft );
-  
-    layer_add_child( window_get_root_layer( s_main_window ), text_layer_get_layer( s_bus_station ) ); 
-  
-    s_next_station = text_layer_create( GRect( 0, 148, 144, 20 ) );
-    text_layer_set_background_color( s_next_station, GColorDarkCandyAppleRed );
-    text_layer_set_text_color( s_next_station, GColorWhite );
-    text_layer_set_font( s_next_station, fonts_get_system_font( FONT_KEY_GOTHIC_14 ) );
+ 
+    create_text_layer( &s_next_station, GRect( 0, 148, 144, 20 ), GColorDarkCandyAppleRed, GColorWhite, FONT_KEY_GOTHIC_14, GTextAlignmentCenter );
     text_layer_set_text( s_next_station, "next" );
-    text_layer_set_text_alignment( s_next_station, GTextAlignmentCenter );
-
-    layer_add_child( window_get_root_layer( s_main_window ), text_layer_get_layer( s_next_station ) ); 
-  
-    s_busses = text_layer_create( GRect( 5, 28, 144, 120 ) );
-    text_layer_set_background_color( s_busses, GColorWhite );
-    text_layer_set_text_color( s_busses, GColorBlack );
-    text_layer_set_font( s_busses, fonts_get_system_font( FONT_KEY_GOTHIC_14 ) );
-    text_layer_set_text( s_busses, "" );
-    text_layer_set_text_alignment( s_bus_station, GTextAlignmentLeft );
     
-    layer_add_child( window_get_root_layer( s_main_window ), text_layer_get_layer( s_busses ) ); 
-      
     GBitmap* h_icon = gbitmap_create_with_resource(RESOURCE_ID_ICON_H);
     s_banner = bitmap_layer_create(GRect(3, 3, 18, 18));
     bitmap_layer_set_compositing_mode(s_banner, GCompOpSet);
@@ -58,14 +110,15 @@ static void main_window_load()
   
     layer_add_child(window_get_root_layer(s_main_window), bitmap_layer_get_layer(s_banner));
     layer_set_update_proc(window_get_root_layer (s_main_window), update_proc);
-      
-
-  
+    
+    create_bus_text_layers();
 }
 
 static void main_window_unload()
 {
-    text_layer_destroy( s_busses );
+    destroy_bus_text_layers();
+    bitmap_layer_destroy( s_banner );
+    text_layer_destroy( s_next_station );
     text_layer_destroy( s_bus_station );
 }
 
@@ -76,9 +129,6 @@ static void inbox_received_callback( DictionaryIterator* iterator, void* context
     Tuple* t = dict_read_first( iterator );
    
     char* bus_stop_name = NULL;
-    char* bus_one = NULL;
-    char* bus_two = NULL;
-    char* bus_three = NULL;
 //    char* bus_stop_dist = NULL;
 //    char* gps_coords = NULL;    
     
@@ -94,15 +144,6 @@ static void inbox_received_callback( DictionaryIterator* iterator, void* context
 //            gps_coords = t->value->cstring;
             break;
             default:
-            case BUS_ONE:
-            bus_one = t->value->cstring;
-            break;
-            case BUS_TWO:
-            bus_two = t->value->cstring;
-            break;
-            case BUS_THREE:
-            bus_three = t->value->cstring;
-            break;
             APP_LOG( APP_LOG_LEVEL_ERROR, "[ACbus] Key %d not recognized!", ( int ) t->key );
             break;
         }
@@ -111,13 +152,6 @@ static void inbox_received_callback( DictionaryIterator* iterator, void* context
     }
  
     text_layer_set_text( s_bus_station, bus_stop_name );
-    
-    char* busses_output = bus_one;
-    busses_output = strcat( busses_output, "\n" );
-    busses_output = strcat( busses_output, bus_two );
-    busses_output = strcat( busses_output, "\n" );
-    busses_output = strcat( busses_output, bus_three );
-    text_layer_set_text( s_busses, busses_output );
 }
 
 static void inbox_dropped_callback( AppMessageResult reason, void* context )
