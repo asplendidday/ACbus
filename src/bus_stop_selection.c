938f45f1cf8aed30ebb53787bcff41872e072166
asplendidday <asplendidday@gmail.com>
2015-10-11 23:39:32 +0200

    CHG: testing SimpleMenuLayer for bus stop selection

--- src/bus_stop_selection.c
+++ src/bus_stop_selection.c
@@ -10,7 +10,11 @@
 
 static Window* s_bus_stop_sel_wnd = NULL;
 static TextLayer* s_bus_stop_sel_title = NULL;
+static TextLayer* s_bus_stop_sel_status = NULL;
 static BitmapLayer* s_bus_stop_sel_banner = NULL;
+static SimpleMenuLayer* s_bus_stop_sel_menu = NULL;
+static SimpleMenuSection s_menu_section;
+static SimpleMenuItem s_menu_entries[ 8 ];
 
 
 //==================================================================================================
@@ -25,10 +29,24 @@ void bus_stop_selection_window_load()
     text_layer_set_text( s_bus_stop_sel_title, "Select bus stop" );
     
     common_create_h_icon( &s_bus_stop_sel_banner, s_bus_stop_sel_wnd );
+    
+    for( int i = 0; i != 8; ++i )
+    {
+        s_menu_entries[ i ] = ( SimpleMenuItem ) { .title = "some bus stop name" };
+    }
+    
+    s_menu_section = ( SimpleMenuSection ) { .num_items = 8, .items = s_menu_entries };
+    s_bus_stop_sel_menu = simple_menu_layer_create( GRect( 0, 24, 144, 140 ), s_bus_stop_sel_wnd, &s_menu_section, 1, NULL );
+    layer_add_child( window_get_root_layer( s_bus_stop_sel_wnd ), simple_menu_layer_get_layer( s_bus_stop_sel_menu ) );
+    
+    common_create_text_layer( &s_bus_stop_sel_status, s_bus_stop_sel_wnd, GRect( 0, 148, 144, 20 ), GColorDarkCandyAppleRed, GColorWhite, FONT_KEY_GOTHIC_14, GTextAlignmentCenter );
+    text_layer_set_text( s_bus_stop_sel_status, "No updates, yet." );
 }
 
 void bus_stop_selection_window_unload()
 {
+    text_layer_destroy( s_bus_stop_sel_status );
+    simple_menu_layer_destroy( s_bus_stop_sel_menu );
     bitmap_layer_destroy( s_bus_stop_sel_banner );
     text_layer_destroy( s_bus_stop_sel_title );
 }
